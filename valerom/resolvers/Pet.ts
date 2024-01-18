3e9536d20cac2ee13e762fadb618114c4e77b7bf
rodrigofbmm <115274224+rodrigofbmm@users.noreply.github.com>
2024-01-18 17:07:06 +0100

    Add files via upload

---
+++ valerom/resolvers/Pet.ts
@@ -0,0 +1,13 @@
+import { GraphQLError } from "graphql";
+import { PersonModel, PersonModelType } from "../db/person.ts";
+import { PetModelType } from "../db/pet.ts";
+
+export const Pet = {
+  owner: async (parent: PetModelType): Promise<PersonModelType> => {
+    const person = await PersonModel.findById(parent.owner).exec();
+    if (!person) {
+      throw new GraphQLError(`No person found with id ${parent.owner}`);
+    }
+    return person;
+  },
+};
